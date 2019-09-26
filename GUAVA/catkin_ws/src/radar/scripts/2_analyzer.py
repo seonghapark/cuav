e1ab1bee8edd6b7caa55127616e0732006f5fe1e
Youngjin Kim <zmfjrxmfjr@naver.com>
2019-09-26 16:30:20 -0400

    analyzing sar code

--- GUAVA/catkin_ws/src/radar/scripts/2_analyzer.py
+++ GUAVA/catkin_ws/src/radar/scripts/2_analyzer.py
@@ -26,21 +26,25 @@ class RadarBinaryParser():
         data = bytearray(self.raw_data)
         # print(len(data))
         # parse the sync and data signal in bytearray
+
+        # length of data is under 2 byte
         if len(data) < 2:
-            return None, None
+            return None, None #there are no data
+        # sync of first data's second bit is over 0
         if (data[0] >> 6) > 0:
-            del data[:1]
+            del data[:1] #delete first and second byte
+        # length of data is not divided by two
         if len(data) % 2 == 1:
-            del data[-1:]
+            del data[-1:] #delete last data
 
         values = []
         sync = []
-        for index in range(0, len(data), 2):
-            high = data[index] & 0x1F
-            low = data[index + 1] & 0x1F
-            values.append(high << 5 | low)
+        for index in range(0, len(data), 2): # read two bytes
+            high = data[index] & 0x1F # last five bits of first byte
+            low = data[index + 1] & 0x1F # last five bits of second byte
+            values.append(high << 5 | low) # concatenate first 5bits + second 5bits = 10 bits
+            #sync is True when if first byte's first 3 bits value are 1
             sync.append(True if (data[index] >> 5) == 1 else False)
-
         self.sync = np.array(sync)
         self.data = np.array(values)
 
@@ -55,14 +59,18 @@ def callback(data):
     # parse text binary file
     parser = RadarBinaryParser(raw_data.data, sr=SAMPLE_RATE)
     sync, data = parser.parse()
+    print('data : ',data.shape)
+    print(data)
+    print('sync : ',sync.shape)
+    print(sync)
     # stacking audio data
     #audio = np.vstack((sync,data))
     #audio = audio.T.astype(np.int16)
     #rospy.loginfo(audio)
 
     wav_data = wav()
-    wav_data.data = data.astype(np.int16)
-    wav_data.sync = sync.astype(np.int16)
+    wav_data.data = data.astype(np.uint8)
+    wav_data.sync = sync.astype(np.uint8)
     wav_data.num = raw_data.num
     wav_data.sr = SAMPLE_RATE
     # Publish Audio Numpy data
