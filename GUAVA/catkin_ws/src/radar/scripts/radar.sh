d2a6f7f13c1debd501f8a68400bd2c110710e55b
ihaeeun <ihaeeun16@gmail.com>
2019-10-15 14:28:30 -0400

    Modify read_data.py to ROS

--- GUAVA/catkin_ws/src/radar/scripts/radar.sh
+++ GUAVA/catkin_ws/src/radar/scripts/radar.sh
@@ -3,8 +3,8 @@
 gnome-terminal -e "roscore"
 sleep 3
 #gnome-terminal -e "make_sar_image.py"
-#gnome-terminal -e "rosrun radar 3_draw.py"
-#sleep 3
+gnome-terminal -e "rosrun radar 3_draw.py"
+sleep 3
 gnome-terminal -e "rosrun radar 2_analyzer.py"
 sleep 3
 #gnome-terminal -e "rosrun radar 0_get_data.py"
