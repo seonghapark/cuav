6e4a7ac107b284012c887a808eca27c0c7f0d66f
Henry <dojinkim119@gmail.com>
2019-10-31 14:24:15 -0400

    run roscore at background

--- GUAVA/catkin_ws/src/camera/scripts/camera.sh
+++ GUAVA/catkin_ws/src/camera/scripts/camera.sh
@@ -3,16 +3,10 @@
 roscore &
 sleep 5
 # gnome-terminal -e "rosrun camera classifier_camera.py"
-# sleep 2
+ sleep 2
 gnome-terminal -e "rosrun camera get_frame.py --config cfg/yolo-drone.cfg --weight weights/yolo-drone.weights --labels cfg/coco-drone.names --conf 0.5 --nms 0.4 --resolution 416"
 sleep 2
 gnome-terminal -e "rosrun camera fake_rail.py"
 sleep 1
 gnome-terminal -e "rosrun camera fake_start.py"
-#roscore &
-#sleep 5
-#rosrun camera get_frame.py --config cfg/yolo-drone.cfg --weight weights/yolo-drone.weights --labels cfg/coco-drone.names --conf 0.5 --nms 0.4 --resolution 416 &
-# sleep 2
-# rosrun camera fake_rail.py &
-# sleep 2
-# rosrun camera fake_start.py &
+
