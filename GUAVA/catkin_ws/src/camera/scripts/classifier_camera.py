1ebd80b1c810090f7a5a3d52e22bb8a6c50e5717
Henry <dojinkim119@gmail.com>
2019-11-05 13:38:27 -0500

    add log convention

--- GUAVA/catkin_ws/src/camera/scripts/classifier_camera.py
+++ GUAVA/catkin_ws/src/camera/scripts/classifier_camera.py
@@ -5,18 +5,31 @@ import rospy
 from std_msgs.msg import String
 from camera.msg import sendframe
 from cv_bridge import CvBridge, CvBridgeError
+from camera_log import log_generator
 
 
 class ClassifierCamera:
 	def __init__(self):
+		self.classify = rospy.Subscriber('img_camera', sendframe, self.callback)
+		self.realtime = rospy.Publisher('realtime_camera', sendframe, queue_size=3)
+		self.summary = rospy.Publisher('summary_camera', sendframe, queue_size=3)
+		self.log = rospy.Publisher('log', String, queue_size=10)
+		self.node_name = "classifier_camera"
 		self.detected_frames = []
 		self.detected_objects = []
 		self.detected_percentages = []
 		self.bridge = CvBridge()
 		self.send_frame = sendframe()
-		self.classify = rospy.Subscriber('img_camera', sendframe, self.callback)
-		self.realtime = rospy.Publisher('realtime_camera', sendframe, queue_size=3)
-		self.summary = rospy.Publisher('summary_camera', sendframe, queue_size=3)
+		self.log.publish(log_generator(self.node_name, "classifier_camera connects ROS"))
+
+	def callback(self, data):
+		if data.operate == "start":
+			self.log.publish(log_generator(self.node_name, "img_camera(rail operating)", "sub"))
+			self.realtime_callback(data)
+		elif data.operate == "end":
+			self.log.publish(log_generator(self.node_name, "img_camera(rail ended)", "sub"))
+			pass
+			# self.summary_callback(data)
 
 	def preprocess_frames(self):
 		try:
@@ -27,16 +40,15 @@ class ClassifierCamera:
 			print("EOF error")
 
 	def realtime_callback(self, frame_data):
-		print("Send frame in realtime")
 		self.realtime.publish(frame_data)
+		self.log.publish(log_generator(self.node_name, "realtime_camera", "pub"))
 		try:
 			cv_image = self.bridge.imgmsg_to_cv2(frame_data.frame, desired_encoding="passthrough")
-			print('"Image converted')
 		except CvBridgeError as e:
 			print(e)
 
 		cv2.imshow("classify Frame", cv_image)
-		cv2.waitKey(30)
+		cv2.waitKey(10)
 
 		# accumulate detected frames + labels + percentages
 		self.detected_frames.append(cv_image)
@@ -44,8 +56,6 @@ class ClassifierCamera:
 		self.detected_percentages.append(frame_data.percent)
 
 	def summary_callback(self, frame_data):
-		print("Send summarized frame")
-		
 		# preprocess summarized data
 		frame, self.send_frame.object, self.send_frame.percent = self.preprocess_frames()
 
@@ -53,6 +63,7 @@ class ClassifierCamera:
 			self.send_frame.operate = frame_data.operate
 			self.send_frame.frame = self.bridge.cv2_to_imgmsg(frame, encoding="passthrough")
 			self.summary.publish(self.send_frame)
+			self.log.publish(log_generator(self.node_name, "summary_camera", "pub"))
 		except CvBridgeError as e:
 			print(e)
 
@@ -63,16 +74,8 @@ class ClassifierCamera:
 		self.detected_objects.clear()
 		self.detected_percentages.clear()
 
-	def callback(self, data):
-		if data.operate == "start":
-			self.realtime_callback(data)
-		elif data.operate == "end":
-			pass
-			# self.summary_callback(data)
-
 
 if __name__ == '__main__':
-	print("Start classifier camera")
 	classifier_camera = ClassifierCamera()
 	rospy.init_node('classifier_camera', anonymous=True)
 	try:
--- GUAVA/catkin_ws/src/camera/scripts/get_frame.py
+++ GUAVA/catkin_ws/src/camera/scripts/get_frame.py
@@ -3,11 +3,12 @@ import cv2
 import sys
 import rospy
 import argparse
-import numpy as np
 from detection_boxes import DetectBoxes
 from std_msgs.msg import String
 from camera.msg import sendframe
 from cv_bridge import CvBridge, CvBridgeError
+from camera_log import log_generator
+
 
 def arg_parse():
     """ Parsing Arguments for detection """
@@ -16,8 +17,10 @@ def arg_parse():
     parser.add_argument("--config", help="Yolov3 config file", default="cfg/yolo-drone.cfg")
     parser.add_argument("--weight", help="Yolov3 weight file", default="weights/yolo-drone.weights")
     parser.add_argument("--labels", help="Yolov3 label file", default="cfg/coco-drone.names")
-    parser.add_argument("--conf", dest="confidence", help="Confidence threshold for predictions", default=0.5)
-    parser.add_argument("--nms", dest="nmsThreshold", help="NMS threshold", default=0.4)
+    parser.add_argument("--conf", dest="confidence", help="Confidence threshold for predictions",
+                        default=0.5, type=float)
+    parser.add_argument("--nms", dest="nmsThreshold", help="NMS threshold",
+                        default=0.4, type=float)
     parser.add_argument("--resolution", dest='resol', help="Input resolution of network. Higher "
                                                       "increases accuracy but decreases speed",
                         default="416", type=str)
@@ -28,40 +31,42 @@ class GetFrame:
     def __init__(self):
         self.operate = rospy.Subscriber('operate', String, self.callback)
         self.send_frame = rospy.Publisher('img_camera', sendframe, queue_size=3)
+        self.log = rospy.Publisher('log', String, queue_size=10)
+        self.node_name = "get_frame"
         self.net = None
         self.detect = None
         self.cap = None
         self.args = arg_parse()
         self.frame_data = sendframe()
         self.bridge = CvBridge()
-        return
+        self.log.publish(log_generator(self.node_name, "get_frame connects ROS"))
 
     def callback(self, data):
-        print("start get_frame -", data.data)
         if data.data == "init":
-            print('initialization')
+            self.log.publish(log_generator(self.node_name, "operate(camera - initialized)", "sub"))
             self.initialize()
         elif data.data == "start":
-            print('start')
+            self.log.publish(log_generator(self.node_name, "operate(rail operating)", "sub"))
             self.get_frame(data)
-        elif data.data == 'end':
-            print('end')
+        elif data.data == "end":
+            self.log.publish(log_generator(self.node_name, "operate(rail ended)", "sub"))
+            self.get_frame(data)
 
-    def initialize(self):     
-        print("Loading network.....")
+    def initialize(self):
+        self.log.publish(log_generator(self.node_name, "Loading network....."))
         self.net = cv2.dnn.readNetFromDarknet(self.args.config, self.args.weight)
         self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
         self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-        print("Network successfully loaded")
+        self.log.publish(log_generator(self.node_name, "Network successfully loaded"))
 
         # load detection class, default confidence threshold is 0.5
-        self.detect = DetectBoxes(self.args.labels, confidence_threshold=float(self.args.confidence), nms_threshold=float(self.args.nmsThreshold))
+        self.detect = DetectBoxes(self.args.labels, confidence_threshold=self.args.confidence, nms_threshold=self.args.nmsThreshold)
         
         try:
             self.cap = cv2.VideoCapture(0)
-            print("Start reading image frames from Webcam")
+            self.log.publish(log_generator(self.node_name, "Webcam initialized"))
         except IOError:
-            print("No Webcam")
+            self.log.publish(log_generator(self.node_name, "No Webcam detected - system exits"))
             sys.exit(1)
 
     @staticmethod
@@ -71,20 +76,19 @@ class GetFrame:
         return [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
     def get_frame(self, operate):
-        print("get_frame")
         # if start signal came before init signal
         if not self.cap.isOpened():
             self.initialize()
 
         while self.cap.isOpened():
-            hasFrame, frame = self.cap.read()
+            has_frame, frame = self.cap.read()
             # if end of frame, program is terminated
-            if not hasFrame:
+            if not has_frame:
                 break
 
             # Create a 4D blob from a frame.
-            blob = cv2.dnn.blobFromImage(frame, 1 / 255, (int(self.args.resol), int(self.args.resol)), (0, 0, 0), True,
-                                        crop=False)
+            blob = cv2.dnn.blobFromImage(frame, 1 / 255, (int(self.args.resol), int(self.args.resol)),
+                                         (0, 0, 0), True, crop=False)
 
             # Set the input to the network
             self.net.setInput(blob)
@@ -108,14 +112,14 @@ class GetFrame:
             try:   
                 self.frame_data.frame = self.bridge.cv2_to_imgmsg(frame, encoding="passthrough")
                 self.send_frame.publish(self.frame_data)
-                print('image published')
+                self.log.publish(log_generator(self.node_name, "img_camera", "pub"))
             except CvBridgeError as e:
                 print(e)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-        print("Camera detection ended")
+        self.log.publish(log_generator(self.node_name,"Camera closed"))
         # releases video and removes all windows generated by the program
         # cap.release()
 
