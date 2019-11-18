8fc346fde5dfacd668e79e6c6344c1c4bc1d65e1
ihaeeun <ihaeeun16@gmail.com>
2019-11-18 15:04:00 -0500

    Modify log

--- GUAVA/catkin_ws/src/radar/scripts/2_analyzer.py
+++ GUAVA/catkin_ws/src/radar/scripts/2_analyzer.py
@@ -34,20 +34,20 @@ class RadarBinaryParser():
         # parse the sync and data signal in bytearray
         # length of data is under 2 byte
         if len(data) < 2:
-            return None, None #there are no data
+            return None, None  # there are no data
         # sync of first data's second bit is over 0
         if (data[0] >> 6) > 0:
-            del data[:1] #delete first and second byte
+            del data[:1]  # delete first and second byte
         # length of data is not divided by two
         if len(data) % 2 == 1:
-            del data[-1:] #delete last data
+            del data[-1:]  # delete last data
 
         values = []
         sync = []
-        for index in range(0, len(data), 2): # read two bytes
-            high = data[index] & 0x1F # last five bits of first byte
-            low = data[index + 1] & 0x1F # last five bits of second byte
-            values.append(high << 5 | low) # concatenate first 5bits + second 5bits = 10 bits
+        for index in range(0, len(data), 2):  # read two bytes
+            high = data[index] & 0x1F  # last five bits of first byte
+            low = data[index + 1] & 0x1F  # last five bits of second byte
+            values.append(high << 5 | low)  # concatenate first 5bits + second 5bits = 10 bits
             # sync is True when if first byte's first 3 bits value are 1
             sync.append(True if (data[index] >> 5) == 1 else False)
         self.sync = np.array(sync)
@@ -59,11 +59,11 @@ class RadarBinaryParser():
 class ifft_handler():
     def __init__(self):
         self.opp = 0
-        #self.fs = 44100  # Sampling rate
+        # self.fs = 44100  # Sampling rate
         self.fs = 5862
-        self.Tp = 0.020   # Radar ramp up-time
-        self.n = int(self.Tp*self.fs)   # Samples per ramp up-time
-        self.fsif = np.zeros([10000,self.n], dtype=np.int16)  # Zero array for further data storage
+        self.Tp = 0.020  # Radar ramp up-time
+        self.n = int(self.Tp * self.fs)  # Samples per ramp up-time
+        self.fsif = np.zeros([10000, self.n], dtype=np.int16)  # Zero array for further data storage
 
     def dbv(self, input):
         input = input + 0.0000001
@@ -71,47 +71,54 @@ class ifft_handler():
 
     def data_process(self, sync, data):
         count = 0
-        result_time = [] # time is a list
+        result_time = []  # time is a list
         self.fs = len(sync)
-        self.n = int(self.Tp*self.fs)
-        self.fsif = np.zeros([10000,self.n], dtype=np.int16)
+        self.n = int(self.Tp * self.fs)
+        self.fsif = np.zeros([10000, self.n], dtype=np.int16)
 
-        spliter = 10 # to search rising edge
+        spliter = 10  # to search rising edge
         val = 2
         for ii in range(val, int((sync.shape[0] - self.n)), spliter):  # improved searching loop
             # if sync[ii - 11 - spliter:ii - spliter] == []:
             #     continue
-            if (ii - spliter > 0) & (sync[ii] == True) & (sync[ii - val - spliter:ii - spliter].max() == False):  # if start[ii] is true and the mean of from start[ii-11] to start[ii-1] is zero (All False)
+            if (ii - spliter > 0) & (sync[ii] == True) & (sync[
+                                                          ii - val - spliter:ii - spliter].max() == False):  # if start[ii] is true and the mean of from start[ii-11] to start[ii-1] is zero (All False)
                 for jj in range(ii - spliter, ii):
                     if (sync[jj] == True) & (sync[jj - val:jj - 1].mean() == 0.0):
-                        self.fsif[count, :] = data[jj:jj + self.n]  # then copy rightarray from ii to ii+n and paste them to sif[count] --> sif[count] is a list
-                        result_time.append((jj + int(sync.shape[0]) * self.opp) * 1. / self.fs)  # append time, the time is ii/fs --> few micro seconds (0.0001 sec or so)
+                        self.fsif[count, :] = data[
+                                              jj:jj + self.n]  # then copy rightarray from ii to ii+n and paste them to sif[count] --> sif[count] is a list
+                        result_time.append((jj + int(sync.shape[
+                                                         0]) * self.opp) * 1. / self.fs)  # append time, the time is ii/fs --> few micro seconds (0.0001 sec or so)
                         count = count + 1
 
-                        self.fsif[count, :] = data[jj + 1:jj + 1 + self.n]  # then copy rightarray from ii to ii+n and paste them to sif[count] --> sif[count] is a list
-                        result_time.append((jj + 1 + int(sync.shape[0]) * self.opp) * 1. / self.fs)  # append time, the time is ii/fs --> few micro seconds (0.0001 sec or so)
+                        self.fsif[count, :] = data[
+                                              jj + 1:jj + 1 + self.n]  # then copy rightarray from ii to ii+n and paste them to sif[count] --> sif[count] is a list
+                        result_time.append((jj + 1 + int(sync.shape[
+                                                             0]) * self.opp) * 1. / self.fs)  # append time, the time is ii/fs --> few micro seconds (0.0001 sec or so)
                         count = count + 1
 
                         break
 
         self.opp += 1
         result_time = np.array(result_time)  # change the format of time from list to to np.array
-        sif = self.fsif[:count,:] # truncate sif --> remove all redundant array lists in sif, just in case if sif is longer then count
+        sif = self.fsif[:count,
+              :]  # truncate sif --> remove all redundant array lists in sif, just in case if sif is longer then count
         sif = sif - np.tile(sif.mean(0), [sif.shape[0], 1])
-        #zpad = int(8 * self.n / 2)  # create the number_of_ifft_entities --> which is the number of vales that has to be created from fft calculation
+        # zpad = int(8 * self.n / 2)  # create the number_of_ifft_entities --> which is the number of vales that has to be created from fft calculation
         zpad = 440
-        decibel = self.dbv(np.fft.ifft(sif, zpad, 1)) # Do fft calculation, and convert results to decibel through dbv function
-        real_value = decibel[:,0:int(decibel.shape[1] / 2)]
+        decibel = self.dbv(
+            np.fft.ifft(sif, zpad, 1))  # Do fft calculation, and convert results to decibel through dbv function
+        real_value = decibel[:, 0:int(decibel.shape[1] / 2)]
         max_real = real_value.max()
         result_data = real_value - max_real
 
         #### 2 pulse cancelor RTI plot
-        sif2 = sif[1:sif.shape[0],:] - sif[:sif.shape[0]-1,:]
-        last = sif[-1,:]
+        sif2 = sif[1:sif.shape[0], :] - sif[:sif.shape[0] - 1, :]
+        last = sif[-1, :]
         sif2 = np.vstack((sif2, last))
         v = np.fft.ifft(sif2, zpad, 1)
         decibel = self.dbv(v)
-        real_value = decibel[:,0:int(decibel.shape[1] / 2)]
+        real_value = decibel[:, 0:int(decibel.shape[1] / 2)]
         max_real = real_value.max()
         result_data = real_value - max_real
 
@@ -142,12 +149,10 @@ def publish_realtime_wav(data):
     if sync is None:
         time.sleep(0.2)
 
-    st = time.time() * 1000
     result_time, result_data = ifft.data_process(sync, real_data)  # It takes approximately 500 ms
-    et = time.time() * 1000
 
     str_time = str(datetime.now()).replace(' ', '_')
-    str_msg = 'After IIFT Data : ' + str(result_data.shape) + ' Sync : ' + str(result_time.shape)
+    str_msg = 'Data : ' + str(result_data.shape) + ' Sync : ' + str(result_time.shape)
     log_text = '[{}/{}][{}] {}'.format(PACKAGE_NAME, NODE_NAME, str_time, str_msg)
     log.publish(log_text)
     print(log_text)
